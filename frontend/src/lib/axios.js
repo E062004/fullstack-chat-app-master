467043feb751fe12a811bddaf6f3e470944abc34
E062004 <131036712+E062004@users.noreply.github.com>
2025-05-12 14:52:32 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,16 @@
-import axios from "axios";
+/*import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "https://fullstack-chat-app-backend-kz2a.onrender.com/api" : "/api",
   withCredentials: true,
+});*/
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api" // when testing locally
+      : "https://fullstack-chat-app-backend-kz2a.onrender.com/api", // on Render
+  withCredentials: true,
 });
+
