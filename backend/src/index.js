32dd86fb49ada2dd21a7de407d2a8307a1e94946
E062004 <131036712+E062004@users.noreply.github.com>
2025-05-12 14:30:05 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://fullstack-chat-app-frontend-umv9.onrender.com",
     credentials: true,
   })
 );
